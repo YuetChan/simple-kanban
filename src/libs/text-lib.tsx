02b29c8ea99ee9d8a4263a7eaea18033de72c492
Yuet Chan <yuetcheukchan@gmail.com>
2022-10-13 08:54:26 -0400

    Deleted next.js project

--- src/libs/text-lib.tsx
+++
@@ -1,5 +0,0 @@
-const truncate = (input, width) => {
-  return input?.length > width ? `${input.substring(0, width)} ...` : input;
-}
-
-export { truncate };
--- src/services/backend-enum-service.tsx
+++
@@ -1,5 +0,0 @@
-const stringToEnum = (str) => {
-  return str.replace(/([A-Z])/g, '_$1').trim().toUpperCase()
-}
-
-export { stringToEnum }
